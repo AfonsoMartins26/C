eea2ca93c70daf3c1051a1ead6cfa428bd40450f
Afonso Martins <a106931@alunos.uminho.pt>
2024-02-21 18:54:04 +0000

    exercicio 3 da ficha 2 feito

--- School/teste2.c
+++ School/teste2.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    int count = 0;
-    int n = 81;
-    for(;n>0 ;n /=2) {
-        count++;
-    }
-    printf("%d", count);
-    return count;
+
+int mdc1 (int a, int b){ // vai fazer o resto aé que o mesmo seja 0 aí o ultimo numero vai ser o mdc 
+    int resto ; 
+    do {
+        resto = a %b;
+
+        a = b;
+        b = resto ;
+
+    }while (resto != 0);
+
+    return a;
+}
+
+int main (){
+    int a ,b ;
+    scanf ("%d,%d" ,&a ,&b ) ;
+    int i =mdc1(a,b);
+    printf("%d" , i);   
 }
