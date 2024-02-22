ecb30a6b7c04a4295fab081af6aafafc3368f132
Afonso Martins <a106931@alunos.uminho.pt>
2024-02-22 14:28:15 +0000

    exercicio 4 da ficha 2 de pi

--- Ficha2.c
+++ Ficha2.c
@@ -86,3 +86,27 @@ int main (){
     int i =mdc1(a,b);
     printf("%d" , i);   
 }
+
+//4 
+int mdc2 (int a, int b){
+    for(int i = 0 ; i < 8 ;i++ ){printf("%d %d\n", a, b);
+    if (a >b) {
+        a = a-b;
+        
+    }
+    else if (a < b){
+        b = b - a;
+    }
+    else if (a == b){
+        return a;
+    }
+    }
+
+}
+
+int main (){
+    int a ,b;
+    scanf ("%d %d" ,&a ,&b ) ;
+    int i = mdc2(a,b);
+    printf("%d" , i);   
+}
