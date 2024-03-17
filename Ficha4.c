8985036ce93ad31eaa0e2f9672f1fca5021a575c
Afonso Martins <a106931@alunos.uminho.pt>
2024-03-17 23:36:26 +0000

    Ficha 4

--- Ficha4.c
+++ Ficha4.c
@@ -69,7 +69,89 @@ void duplicaVogais(char *s) {
 
 //1 
 
-int ordenado (int v[], int N){
-    
+int ordenado(int v[], int N) {
+    for (int i = 0; i < N - 1; i++) {
+        if (v[i] > v[i + 1]) {
+            return 1; 
+        }
+    }
+    return 0; 
 }
+
  
+//2
+
+void merge(int r[], int a[], int b[], int na, int nb) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < na && j < nb) {
+        if (a[i] <= b[j]) {
+            r[k++] = a[i++];
+        } else {
+            r[k++] = b[j++];
+        }
+    }
+
+    while (i < na) {
+        r[k++] = a[i++];
+    }
+
+    while (j < nb) {
+        r[k++] = b[j++];
+    }
+}
+
+//3
+
+int partition_aux(int v[], int aux[], int N, int x) {
+    int i, j;
+    int count = 0;
+
+    for (i = 0; i < N; i++) {
+        if (v[i] <= x) {
+            aux[count++] = v[i];
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        if (v[i] > x) {
+            aux[count++] = v[i];
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        v[i] = aux[i];
+    }
+
+    return count;
+}
+
+int partition(int v[], int N, int x) {
+    int aux[N];
+    return partition_aux(v, aux, N, x);
+}
+
+//versao alternativa sem uso de uma array auxiliar 
+
+int partition1(int v[], int N, int x) {
+    int a = 0; 
+    int b = N - 1; 
+
+    while (a <= b) {
+        if (v[a] <= x) {
+            a++;
+        } else if (v[b] > x) {
+            b--;
+        } else {
+            int temp = v[a];
+            v[a] = v[b];
+            v[b] = temp;
+            a++;
+            b--;
+        }
+    }
+
+    return a;
+}
+
+
